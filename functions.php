d4e7f773b9dadabb554281cfc44c9efadda01586
Jan <janhajk@gmail.com>
2020-12-29 10:21:05 +0000

    bug

--- functions.php
+++ functions.php
@@ -183,7 +183,7 @@ function wcs_custom_get_availability( $availability, $_product ) {
 remove_action('woocommerce_shop_loop_item_title','woocommerce_template_loop_product_title',10);
 add_action('woocommerce_shop_loop_item_title','sot_custom_loop_title',10);
 function sot_custom_loop_title() {
-   $MAX_LEN = 35;
+   $MAX_LEN = 60;
    global $product;
    $title = get_the_title();
    $len = strlen($title);
@@ -192,7 +192,7 @@ function sot_custom_loop_title() {
       if ($len >= $MAX_LEN) {
          $title .= '...';
    }
-   echo '<h3 class="' . esc_attr( apply_filters( 'woocommerce_product_loop_title_classes', 'woocommerce-loop-product__title' ) ) . '"><a href="' . esc_url( $link ) . '" class="woocommerce-LoopProduct-link-title woocommerce-loop-product__title_ink">' . $title . '</a></h3>';
+   echo '<h3 class="' . esc_attr( apply_filters( 'woocommerce_product_loop_title_classes', 'woocommerce-loop-product__title' ) ) . '" style="height:55px"><a href="' . esc_url( $link ) . '" class="woocommerce-LoopProduct-link-title woocommerce-loop-product__title_ink">' . $title . '</a></h3>';
 }
 
 ?>
