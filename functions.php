e29b9ebee9cec81a95fc89d524fc273ad9780677
Jan <janhajk@gmail.com>
2020-12-29 10:18:33 +0000

    bug

--- functions.php
+++ functions.php
@@ -189,6 +189,9 @@ function sot_custom_loop_title() {
    $len = strlen($title);
    $link = apply_filters( 'woocommerce_loop_product_link', get_the_permalink(), $product );
    $title = substr($title, 0, $MAX_LEN);
+      if ($len >= $MAX_LEN) {
+         $title .= '...';
+   }
    echo '<h3 class="' . esc_attr( apply_filters( 'woocommerce_product_loop_title_classes', 'woocommerce-loop-product__title' ) ) . '"><a href="' . esc_url( $link ) . '" class="woocommerce-LoopProduct-link-title woocommerce-loop-product__title_ink">' . $title . '</a></h3>';
 }
 
