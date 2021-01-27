2e85960c7cc5b28feb86f73d671bd2ae5cc88d0a
Jan <janhajk@gmail.com>
2021-01-27 16:31:46 +0000

    add cart item number

--- functions.php
+++ functions.php
@@ -269,4 +269,87 @@ function sot_custom_price_exkl_mwst($price, $instance) {
     return $price . '<br/><span class="woocommerce-Price-amount amount"><bdi><span class="woocommerce-Price-currencySymbol">CHF</span>'.round(wc_get_price_excluding_tax($instance),2).' </bdi></span><small class="woocommerce-price-suffix">exkl. MWST</small>';
 }
 
+
+
+
+/**
+ * 
+ * 
+ * Show number of items in cart 
+ *
+ * 
+ * 
+ * 
+ * 
+ */
+
+
+
+
+
+add_shortcode ('woo_cart_but', 'woo_cart_but' );
+/**
+ * Create Shortcode for WooCommerce Cart Menu Item
+ */
+function woo_cart_but() {
+        ob_start();
+
+        $cart_count = WC()->cart->cart_contents_count; // Set variable for cart item count
+        $cart_url = wc_get_cart_url();  // Set Cart URL
+
+        ?>
+        <li><a class="menu-item cart-contents" href="<?php echo $cart_url; ?>" title="Warenkorb">
+            <?php
+        if ( $cart_count > 0 ) {
+       ?>
+            <span class="cart-contents-count"><?php echo $cart_count; ?></span>
+        <?php
+        }
+        ?>
+        </a></li>
+        <?php
+
+    return ob_get_clean();
+
+}
+
+add_filter( 'woocommerce_add_to_cart_fragments', 'woo_cart_but_count' );
+/**
+ * Add AJAX Shortcode when cart contents update
+ */
+function woo_cart_but_count( $fragments ) {
+
+    ob_start();
+
+    $cart_count = WC()->cart->cart_contents_count;
+    $cart_url = wc_get_cart_url();
+
+    ?>
+    <a class="cart-contents menu-item" href="<?php echo $cart_url; ?>" title="<?php _e( 'Warenkorb' ); ?>">
+        <?php
+    if ( $cart_count > 0 ) {
+        ?>
+        <span class="cart-contents-count"><?php echo $cart_count; ?></span>
+        <?php
+    }
+
+    $fragments['a.cart-contents'] = ob_get_clean();
+
+    return $fragments;
+}
+add_filter( 'wp_nav_menu_top-menu_items', 'woo_cart_but_icon', 10, 2 ); // Change menu to suit - example uses 'top-menu'
+
+/**
+ * Add WooCommerce Cart Menu Item Shortcode to particular menu
+ */
+function woo_cart_but_icon ( $items, $args ) {
+       $items .=  '[woo_cart_but]'; // Adding the created Icon via the shortcode already created
+
+       return $items;
+}
+
+
+
+
+
 ?>
