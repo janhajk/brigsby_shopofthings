f73713e5826490d1745b3de3935553a4d12a5aec
Jan <janhajk@gmail.com>
2021-03-20 09:25:29 +0000

    add b2b script

---
+++ b2b.js
@@ -0,0 +1,25 @@
+document.addEventListener('DOMContentLoaded', function() {
+      console.log('B2B loading...');
+});
+
+
+
+
+
+/*
+function ReadCookie()
+{
+   var allcookies = document.cookie;
+   alert("All Cookies : " + allcookies );
+
+   // Get all the cookies pairs in an array
+   cookiearray  = allcookies.split('; '); //notes the space after ;
+
+   // Now take key value pair out of this array
+   for(var i=0; i<cookiearray.length; i++){
+       name = cookiearray[i].split('=')[0];
+       value = cookiearray[i].split('=')[1];
+       alert("Key is : " + name + " and Value is : " + value);
+   }
+}
+*/
--- functions.php
+++ functions.php
@@ -405,6 +405,22 @@ function woo_cart_but_icon ( $items, $args ) {
 
 
 
+/**
+ *
+ * Add B2B Javascript
+ *
+ *
+ */
+function shopofthings_add_b2b_script() {
+      // Register js file
+      wp_register_script( 'shopofthings-b2b', get_template_directory_uri() .'/b2b.js', false, '0.1', true );
+
+      // Enqueue the registered script file
+      wp_enqueue_script('shopofthings-b2b');
+}
+
+// add script
+add_action('wp_enqueue_scripts', 'shopofthings_add_b2b_script');
 
 
 ?>
