3cf3c8f699ffb4703e0b2b05c1bd7eb137a78a45
Jan <janhajk@gmail.com>
2021-03-30 17:57:21 +0000

    add link to tracking number bug

--- functions.php
+++ functions.php
@@ -540,11 +540,11 @@ function sot_order_view_add_tracking( $order_id ){
 
 // display the extra data in the order admin panel
 function sot_display_tracking( $order ){  ?>
-    <?php $tn = get_post_meta( $order->id, 'shopofthings_sendungsnummer', true );
+    <?php $tn = get_post_meta( $order->id, 'shopofthings_sendungsnummer', true ); ?>
     <?php if ($tn) { ?>
           <div class="order_data_column">
               <h4><?php __( 'Versand' ); ?></h4>
-              
+
               <?php echo '<p><strong>' . __( 'Sendungsnummer' ) . ':</strong>' . '<a href="https://service.post.ch/ekp-web/ui/entry/search/' . $tn  .'" target="_blank">' . $tn  . '</a></p>'; ?>
           </div>
     <?php } ?>
