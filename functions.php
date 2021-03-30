1a0715c3bcbadf57e070f1a5a83afaf23fb98615
Jan <janhajk@gmail.com>
2021-03-30 15:39:37 +0000

    bug

--- functions.php
+++ functions.php
@@ -520,7 +520,6 @@ add_action('wp_enqueue_scripts', 'shopofthings_add_b2b_script');
  *
  *
  */
-<?php
 add_action( 'woocommerce_view_order', 'sot_order_view_add_tracking', 20 );
 
 function sot_order_view_add_tracking( $order_id ){
@@ -539,7 +538,15 @@ function sot_order_view_add_tracking( $order_id ){
 }
 
 
-
+// display the extra data in the order admin panel
+function sot_display_tracking( $order ){  ?>
+    <div class="order_data_column">
+        <h4><?php __( 'Versand' ); ?></h4>
+        <?php
+            echo '<p><strong>' . __( 'Sendungsnummer' ) . ':</strong>' . get_post_meta( $order->id, 'shopofthings_sendungsnummer', true ) . '</p>'; ?>
+    </div>
+<?php }
+add_action( 'woocommerce_admin_order_data_after_order_details', 'sot_display_tracking' );
 
 
 ?>
