9357651bc9d0b74c4d56b7c652889974ad4f5e59
Jan <janhajk@gmail.com>
2021-06-07 07:51:15 +0000

    remove h3

--- functions.php
+++ functions.php
@@ -618,10 +618,10 @@ function sot_order_view_add_credentials( $order_id ){
  *
  */
 
- add_action('woocommerce_before_bundled_items', 'sot_bundled_options_title', 20);
- function sot_bundled_options_title() {
-       echo '<h3 style="border-bottom:1px solid rgba(0,0,0,0.1);padding-bottom:10px;">Zusätzliche Optionen</h3>';
- }
+//  add_action('woocommerce_before_bundled_items', 'sot_bundled_options_title', 20);
+//  function sot_bundled_options_title() {
+//        echo '<h3 style="border-bottom:1px solid rgba(0,0,0,0.1);padding-bottom:10px;">Zusätzliche Optionen</h3>';
+//  }
 
 
 
