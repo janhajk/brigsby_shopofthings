69da58fc352e289eda829cf923d30b661342b89d
Jan <janhajk@gmail.com>
2021-07-01 18:44:20 +0000

    add devices page to my account (still empty)

--- functions.php
+++ functions.php
@@ -544,7 +544,7 @@ add_action( 'woocommerce_view_order', 'sot_order_view_add_tracking', 20 );
 add_action( 'woocommerce_admin_order_data_after_order_details', 'sot_display_tracking' );
 
 function sot_order_view_add_tracking( $order_id ){
-      $POST_LINK = "https://service.post.ch/ekp-web/ui/entry/search/";
+    $POST_LINK = "https://service.post.ch/ekp-web/ui/entry/search/";
     $metafield = get_post_meta( $order_id, 'shopofthings_sendungsnummer', true );
     if (empty($metafield)) return;
     $metafield = json_decode($metafield, true);
@@ -736,5 +736,65 @@ function sot_after_add_to_cart_form_connectivity(){
             }
       }
 }
+
+
+
+
+
+
+
+
+
+
+/**
+ * @snippet       WooCommerce Add New Tab @ My Account
+ * @how-to        Get CustomizeWoo.com FREE
+ * @author        Rodolfo Melogli
+ * @compatible    WooCommerce 5.0
+ */
+  
+// ------------------
+// 1. Register new endpoint (URL) for My Account page
+// Note: Re-save Permalinks or it will give 404 error
+  
+function sot_add_devices_endpoint() {
+    add_rewrite_endpoint( 'devices', EP_ROOT | EP_PAGES );
+}
+  
+add_action( 'init', 'sot_add_devices_endpoint' );
+  
+// ------------------
+// 2. Add new query var
+  
+function sot_devices_query_vars( $vars ) {
+    $vars[] = 'devices';
+    return $vars;
+}
+  
+add_filter( 'query_vars', 'sot_devices_query_vars', 0 );
+  
+// ------------------
+// 3. Insert the new endpoint into the My Account menu
+  
+function sot_add_devices_link_my_account( $items ) {
+    $items['devices'] = 'My Devices';
+    return $items;
+}
+  
+add_filter( 'woocommerce_account_menu_items', 'sot_add_devices_link_my_account' );
+  
+// ------------------
+// 4. Add content to the new tab
+  
+function sot_devices_content() {
+   echo '<h3>Your Devices</h3><p>coming soon...</p>';
+   echo do_shortcode( ' /* your shortcode here */ ' );
+}
+  
+add_action( 'woocommerce_account_devices_endpoint', 'sot_devices_content' );
+// Note: add_action must follow 'woocommerce_account_{your-endpoint-slug}_endpoint' format
+
+
+
 
 ?>
