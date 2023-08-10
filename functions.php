0667b2dd6c4314aac83bcc8d1c7c00d00da1e01d
janhajk <janhajk@gmail.com>
2023-08-10 07:11:15 +0000

    hide sidebar in mobile;screen size css var

--- functions.php
+++ functions.php
@@ -34,6 +34,22 @@ add_action( 'after_setup_theme', 'sot_add_woocommerce_support' );
 
 
 
+/**
+ *
+ *
+ *
+ *
+ *
+ */
+function sot_enqueue_styles() {
+    // Registrieren und einbinden der zusätzlichen CSS-Datei
+    wp_enqueue_style('sot-single-product', get_stylesheet_directory_uri() . '/woocommerce/single-product/styles.css', array(), '1.0.0', 'all');
+    wp_enqueue_style('sot-landing-page', get_stylesheet_directory_uri() . '/style.landing-page.css', array(), '1.0.0', 'all');
+}
+
+add_action('wp_enqueue_scripts', 'sot_enqueue_styles');
+
+
 // require_once '/var/www/vhosts/jan/shopofthings/wordpress/wp-content/geoip/vendor/autoload.php';
 // use GeoIp2\Database\Reader;
 function createAdsenseBlogResponsive() {
