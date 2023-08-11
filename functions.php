389908186d7603803dcfcffc9c79494617e7320e
janhajk <janhajk@gmail.com>
2023-08-11 17:23:20 +0000

    landing page beautify and make editable: rearrange files

--- functions.php
+++ functions.php
@@ -44,7 +44,7 @@ add_action( 'after_setup_theme', 'sot_add_woocommerce_support' );
 function sot_enqueue_styles() {
     // Registrieren und einbinden der zusätzlichen CSS-Datei
     wp_enqueue_style('sot-single-product', get_stylesheet_directory_uri() . '/woocommerce/single-product/styles.css', array(), '1.0.16', 'all');
-    wp_enqueue_style('sot-landing-page', get_stylesheet_directory_uri() . '/page-templates/template.landing-page.css', array(), '1.0.1', 'all');
+    wp_enqueue_style('sot-landing-page', get_stylesheet_directory_uri() . '/page-templates/template-landing-page.css', array(), '1.0.2', 'all');
 }
 
 add_action('wp_enqueue_scripts', 'sot_enqueue_styles');
