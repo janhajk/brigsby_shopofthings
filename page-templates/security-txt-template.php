8128b98b1d289cb8dacf800f7ba18b2b57ad2d6a
janhajk <janhajk@gmail.com>
2023-11-23 11:03:34 +0000

    add security.txt template

---
+++ page-templates/security-txt-template.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Template Name: Security.txt
+*/
+header('Content-Type: text/plain');
+?>
+# Our security address (email)
+Contact: admin@thingware.ch
+
+# Preferred languages
+Preferred-Languages: en, de
