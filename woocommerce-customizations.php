f68db8d1248b411e72f23a303087b12a72373e05
janhajk <janhajk@gmail.com>
2024-06-10 19:39:19 +0000

    partner icon: add brands

--- woocommerce-customizations.php
+++ woocommerce-customizations.php
@@ -249,7 +249,9 @@ function sot_show_product_meta_custom() {
     //   }
     // Marke Anzeige
     $marke = $product->get_attribute('pa_brand');
-    $partner_brands = array('elsys', 'rakwireless');  // Array der Partner-Marken
+    $partner_brands = array('elsys', 'rakwireless', 'abeeway', 'adnexo', 'atim', 'circuitmess', 'decentlab', 'reolink', 'seeedstudio', 'small-data-garden',
+    'aal', 'strega', 'swisscom', 'tektelic', 'lilygo-ttgo', 'adeunis', 'digital-matter', 'enginko', 
+    'mclimate', 'milesight', 'miromico', 'nanosensorics', 'netvox', 'panorama', 'teltonika');  // Array der Partner-Marken
     
     if ($marke) {
         $marke_link = get_term_link($marke, 'pa_brand');  // Erstellt einen Link zur Marke
